afde0d0503d856bff4ee881c348fa6244ef34001
Saad Belcadi <saadbelcadi01@gmail.com>
2026-08-01 19:16:34 +0100

    Get arguments.

--- codexion.h
+++ codexion.h
@@ -6,8 +6,23 @@
 # include <unistd.h>
 # include <fcntl.h>
 
+typedef struct  t_arg
+{
+    int nb_coders;
+    int time_to_burnout;
+    int time_to_compile;
+    int time_to_debug;
+    int time_to_refactor;
+    int nb_of_compiles;
+    int dongle_cooldown;
+    int scheduler;
+
+}t_arg;
+
+
 int	ft_atoi(const char	*str);
 int	ft_strcmp(const char	*s1, const char	*s2);
 int ft_strlen(const char    *str);
+void get_arguments(char **argv, t_arg    *args);
 
 #endif
--- parser.c
+++ parser.c
@@ -10,13 +10,13 @@ int in_range_int(char   *s)
     {
         nb = nb * 10 + (s[i] - '0');
 
-        if (nb > 2147483647)
+        if (nb > 2147483647 || nb == 0)
         {
             return (0);
         }
         i++;
     }
-    return (1);
+    return nb;
 }
 
 int check_is_valid_number(char  *s)
@@ -33,7 +33,7 @@ int check_is_valid_number(char  *s)
     return (1);
 }
 
-void parse_arguments(int argc, char **argv)
+void check_arguments(int argc, char **argv)
 {
     int i;
     i = 1;
@@ -45,14 +45,14 @@ void parse_arguments(int argc, char **argv)
     }
     while (i < 8)
     {
-        if (!check_is_valid_number(argv[i]))
+        if (!check_is_valid_number(argv[i]) )
         {
-            printf("The argument number %d isn't valid it should be an positive integer.", i + 1);
+            printf("The argument number %d isn't valid, it should be an positive integer.", i + 1);
             exit(-1);
         }
         if (!in_range_int(argv[i]))
         {
-            printf("The argument number %d exceeds the max int value.\n", i);
+            printf("The argument number %d should be between 1 and MAX_INT.\n", i);
             exit(-1);
         }
         i++;
@@ -64,8 +64,25 @@ void parse_arguments(int argc, char **argv)
     }  
 }
 
+void get_arguments(char **argv, t_arg    *args)
+{
+    args->nb_coders = ft_atoi(argv[1]);
+    args->time_to_burnout = ft_atoi(argv[2]);
+    args->time_to_compile = ft_atoi(argv[3]);
+    args->time_to_debug = ft_atoi(argv[4]);
+    args->time_to_refactor = ft_atoi(argv[5]);
+    args->nb_of_compiles = ft_atoi(argv[6]);
+    args->dongle_cooldown = ft_atoi(argv[7]);
+    if (ft_strcmp(argv[8], "fifo") == 0)
+        args->scheduler = 0;
+    else
+        args->scheduler = 1;
+}
+
 int main(int argc, char **argv)
 {
-    parse_arguments(argc, argv);
+    t_arg   *arguments;
+    check_arguments(argc, argv);
+    get_arguments(argv, arguments);
     return 0;
 }
